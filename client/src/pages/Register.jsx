e2121fd2e4a84e5b93dac102248228df99be21f5
Stef De Jonghe <stef.dejonghe@azstlucas.be>
2023-11-10 15:17:19 +0100

    add navigation state to register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -17,6 +17,9 @@ export const action = async ({ request }) => {
 };
 
 const Register = () => {
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isSubmitting = navigation.state === "submitting";
   return (
     <Wrapper>
       <Form method="post" className="form" action="">
@@ -32,8 +35,8 @@ const Register = () => {
         <FormRow type="text" name="location" defaultValue="earth" />
         <FormRow type="email" name="email" defaultValue="john@gmail.com" />
         <FormRow type="password" name="password" defaultValue="secret123" />
-        <button type="submit" className="btn btn-block">
-          submit
+        <button type="submit" className="btn btn-block" disabled={isSubmitting}>
+          {isSubmitting ? "submitting..." : "submit"}
         </button>
         <p>
           Already a member?
