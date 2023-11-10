4e98e095a1c302e967426fb6e457e93114c4f734
Stef De Jonghe <stef.dejonghe@azstlucas.be>
2023-11-10 15:25:46 +0100

    use react toastify to show messages in register

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -2,6 +2,7 @@ import { Form, redirect, useNavigation, Link } from "react-router-dom";
 import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 import { FormRow, Logo } from "../components";
 import customFetch from "../utils/customFetch";
+import { toast } from "react-toastify";
 
 export const action = async ({ request }) => {
   const formData = await request.formData();
@@ -9,9 +10,10 @@ export const action = async ({ request }) => {
 
   try {
     await customFetch.post("/auth/register", data);
+    toast.success("Registration successful");
     return redirect("/login");
   } catch (error) {
-    console.log(error);
+    toast.error(error?.response?.data.msg);
     return error;
   }
 };
