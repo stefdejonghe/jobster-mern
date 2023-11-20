b592a36d5fa99b44f75263af2782cb9a4aa70879
Stef De Jonghe <stef.dejonghe@azstlucas.be>
2023-11-20 10:48:28 +0100

    add working Admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -25,6 +25,7 @@ import { action as deleteJobAction } from "./pages/DeleteJob";
 import { loader as dashboardLoader } from "./pages/DashboardLayout";
 import { loader as allJobsLoader } from "./pages/AllJobs";
 import { loader as editJobLoader } from "./pages/EditJob";
+import { loader as adminLoader } from "./pages/Admin";
 
 export const checkDefaultTheme = () => {
   const isDarkTheme = localStorage.getItem("darkTheme") === "true";
@@ -80,6 +81,7 @@ const router = createBrowserRouter([
           {
             path: "admin",
             element: <Admin />,
+            loader: adminLoader,
           },
           {
             path: "edit-job/:id",
